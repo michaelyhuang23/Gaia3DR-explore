4d1057f7933e87c354dfe63ac2bbdf060f37e50f
Michael Huang <michaelyhuang23@gmail.com>
2022-08-01 02:53:55 -0400

    m

--- train_egnn.py
+++ train_egnn.py
@@ -60,10 +60,10 @@ if __name__ == '__main__':
     df = df_.iloc[sample_ids].copy()
 
     print(df.columns)
-    # feature_columns = ['estar', 'lzstar', 'lxstar', 'lystar', 'jzstar', 'jrstar', 'eccstar', 'rstar', 'feH', 'mgfe', 'zstar', 'vrstar', 'vphistar', 'vthetastar', 'omegaphistar', 'omegarstar', 'omegazstar', 'thetaphistar', 'thetarstar', 'thetazstar', 'zmaxstar']
+    feature_columns = ['estar', 'lzstar', 'lxstar', 'lystar', 'jzstar', 'jrstar', 'eccstar', 'rstar', 'feH', 'mgfe', 'zstar', 'vrstar', 'vphistar', 'vthetastar', 'omegaphistar', 'omegarstar', 'omegazstar', 'thetaphistar', 'thetarstar', 'thetazstar', 'zmaxstar']
     # feature_columns = ['feH', 'estar', 'lzstar', 'eccstar', 'vthetastar', 'thetazstar', 'jrstar', 'thetaphistar', 'vrstar', 'lystar']
     # feature_columns = ['estar', 'feH', 'c_lzstar', 'jzstar', 'mgfe', 'vrstar', 'zstar', 'vphistar', 'eccstar']
-    feature_columns = ['estar', 'feH', 'lzstar', 'lystar', 'lxstar', 'jzstar', 'jrstar', 'mgfe','eccstar', 'zstar']
+    # feature_columns = ['estar', 'feH', 'lzstar', 'lystar', 'lxstar', 'jzstar', 'jrstar', 'mgfe','eccstar', 'zstar']
     
     # feature_weights = np.array([0.71216923,0.555757,0.31106377,0.1477975,0.13819067,0.1145066,0.08163675,0.07823427,0.07169756])
     # feature_weights /= np.mean(feature_weights)
@@ -146,7 +146,7 @@ if __name__ == '__main__':
                 # print(np.mean(SX.numpy()), np.std(SX.numpy()))
                 torch.save(model, f'weights/m12i_dense_small_model_32_32_epoch{epoch}.pth')
 
-        if epoch % 10 == 0:
+        if epoch % 5 == 0:
             sample_size = 1000
             sample_size = min(sample_size, len(df_))
             sample_ids = np.random.choice(len(df_), min(len(df_), sample_size), replace=False)
