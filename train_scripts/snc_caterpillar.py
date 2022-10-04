3ff2d2da268fca84f39a5cbf6a2e01a781aebe70
Michael Huang <michaelyhuang23@gmail.com>
2022-10-04 16:11:17 -0400

    edit

--- train_scripts/snc_caterpillar.py
+++ train_scripts/snc_caterpillar.py
@@ -5,6 +5,7 @@ import json
 import numpy as np
 import pandas as pd
 import torch
+from torch.utils.tensorboard import SummaryWriter
 
 from tools.cluster_analysis import *
 from tools.neural_dataset import *
@@ -21,7 +22,6 @@ dataset = GraphDataset(feature_columns, cluster_ids='cluster_id', scales=None, k
 snc_clusterer = C_SNC(len(feature_columns), 50, similar_weight=1, egnn_lr=0.01, clustergen_lr=0.01, clustergen_regularizer=0.00001, device='cpu')
 
 trainer = CaterpillarTrainer(snc_clusterer, dataset, 1000000, val_size=4, k_fold=6)
-
 
 EPOCH = 100
 for epoch in range(EPOCH):
