45b22befb139272d891d4e962c5bcf1d648790d2
Michael Huang <michaelyhuang23@gmail.com>
2022-10-04 16:14:41 -0400

    add gpu

--- train_scripts/snc_caterpillar.py
+++ train_scripts/snc_caterpillar.py
@@ -15,11 +15,12 @@ from tools.train_caterpillar import *
 
 
 writer = SummaryWriter()
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 feature_columns = ['estar', 'jrstar', 'jzstar', 'jphistar']
 
 dataset = GraphDataset(feature_columns, cluster_ids='cluster_id', scales=None, knn=10, normalize=True, discretize=False)
-snc_clusterer = C_SNC(len(feature_columns), 50, similar_weight=1, egnn_lr=0.01, clustergen_lr=0.01, clustergen_regularizer=0.00001, device='cpu')
+snc_clusterer = C_SNC(len(feature_columns), 50, similar_weight=1, egnn_lr=0.01, clustergen_lr=0.01, clustergen_regularizer=0.00001, device=device)
 
 trainer = CaterpillarTrainer(snc_clusterer, dataset, 1000000, val_size=4, k_fold=6)
 
